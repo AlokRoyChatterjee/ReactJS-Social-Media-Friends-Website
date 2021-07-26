b9143e229c9b3a5950db4846e4739ce83c507864
ar135 <archat71@gmail.com>
2021-07-26 16:05:28 -0400

    changes

---
+++ data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Name 1',
+    age: 25,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Name 2',
+    age: 28,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Name 3',
+    age: 27,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Name 4',
+    age: 28,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Name 5',
+    age: 28,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
